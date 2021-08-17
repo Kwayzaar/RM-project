70f108ddf8613f1ecd561cd70800073e8d13013f
eric <lee.eric.03@gmail.com>
2021-08-17 16:20:21 -0600

    attempts pagination, still a wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import Header from '../src/components/Header'
 import CharacterList from './components/characters/CharacterList'
 
 function App() {
+  const [characters, setCharacter] = useState([])  
+  
+  const parse = response => response.json()
+  //addToState drills right down to results array and adds to state 
+  const addToState = ({results}) => setCharacter(results)
+  //The call to character url only returns first 20 characters 
+  const characterURL = "https://rickandmortyapi.com/api/character/?page=1"
+  
+  //Use Effect to make the fetch
+  useEffect(() => {
+    fetch(characterURL)
+    .then(parse)
+    .then(addToState)
+  }, [])
+
   return (
-    <div className="App">
+    <div>
       <Header />
-      <CharacterList />
+      <CharacterList characters={ characters } />
     </div>
   );
 }
--- src/components/characters/CharacterList.js
+++ src/components/characters/CharacterList.js
@@ -1,33 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import CharacterCard from './CharacterCard'
 
-function CharacterList() {
-  const [characters, setCharacter] = useState([])
+function CharacterList({ characters }) {
+  const [pageNumber, setPageNumber] = useState(1)
   
   
-  const parse = response => response.json()
-  //addToState drills right down to results array and adds to state 
-  const addToState = ({results}) => setCharacter(results)
-  //The call to character url only returns first 20 characters 
-  const characterURL = "https://rickandmortyapi.com/api/character"
-  
-  //Use Effect to make the fetch
-  useEffect(() => {
-    fetch(characterURL)
-    .then(parse)
-    .then(addToState)
-  }, [])
-  
+  function paginate(array, page_size, page_number) {
+    return characters.slice(page_number * page_size, page_number * page_size + page_size)
+  }
+
+  // ATTEMPT AT PAGINATION
+  // const createCard = () => characterArray.slice(0,9).map(character => {
+  //   return <CharacterCard character={ character } key={ character.id } />
+  // })
+
+  //ORIGINAL FUNCTION FOR RENDERING CARDS 
   //This function maps ("breaks open") characters object (state) and passes it down to CharacterCard. Then, for each character in state, it returns CharacterCard with a key prop attached 
   const createCard = () => characters.map(character => { 
-    return <CharacterCard character={ character } key={ character.id } />
+    return (
+      <CharacterCard character={ character } key={ character.id } />
+    )
   })
+
+
 
   return (
     <div className="w-full h-1/4 p-1.5 bg-gray-100">
       <ul className="flex flex-row flex-wrap">
         {createCard()}
       </ul>
+      <button type="button" >next</button> 
     </div>
   )
 }
