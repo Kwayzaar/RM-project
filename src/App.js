87b3512b4064b3199e31081e60c9b9068ec6f6f0
eric <lee.eric.03@gmail.com>
2021-08-21 07:27:49 -0600

    adds pagination function, function buggy needs work

--- src/App.js
+++ src/App.js
@@ -4,32 +4,39 @@ import Header from '../src/components/Header'
 import CharacterList from './components/characters/CharacterList'
 
 function App() {
-  const [characters, setCharacter] = useState([])
-  const [pageNumber, setPageNumber] = useState(1)
+  const [characters, setCharacters] = useState([])
+  const [groupNumber, setGroupNumber] = useState(1)
   
   const parse = response => response.json()
   //addToState drills right down to results array and adds to state 
-  const addToCharacters = ({results}) => setCharacter(results)
+  const addToCharacters = ({results}) => setCharacters(results)
   //The call to character url only returns first 20 characters 
-  const characterURL = `https://rickandmortyapi.com/api/character?page=${pageNumber}`
   
-  const fetchCharacters = () => {
-    fetch(characterURL)
-    .then(parse)
-    .then(addToCharacters)
-  }
+  function paginatedFetch(
+    characterURL = `https://rickandmortyapi.com/api/character/`,
+    page = 1,
+    previousResponse = []
+  ) {
+      return fetch(`${characterURL}?page=${page}`)
+        .then(parse)
+        .then(newResponse => {
+          // console.log(newResponse.results)
+          const response = previousResponse.push(newResponse.results)
+          
+          if (response.length !== 0) {
+            page++
+            
+            // return paginatedFetch(characterURL, page)
+          }
+
+          return setCharacters(response)
+          
+        })
+    }
 
   //Use Effect to make the fetch
   useEffect(() => {
-    function fetchAllPages(pageNumber) {
-      if (pageNumber < 1) {
-        return []
-      } else {
-        fetchCharacters()
-      }
-    }
-
-    fetchAllPages()
+    paginatedFetch()
   }, [])
 
   return (
