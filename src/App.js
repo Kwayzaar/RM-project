4898984150fcd3058ea881ed8a53c8222f5428e9
Eric <lee.eric.03@gmail.com>
2021-08-26 11:25:25 -0600

    adding last page function

--- src/App.js
+++ src/App.js
@@ -5,12 +5,8 @@ import CharacterList from './components/characters/CharacterList'
 
 function App() {
   const [characters, setCharacters] = useState([])
-  const [groupNumber, setGroupNumber] = useState(1)
   
   const parse = response => response.json()
-  //addToState drills right down to results array and adds to state 
-  const addToCharacters = ({results}) => setCharacters(results)
-  //The call to character url only returns first 20 characters 
   
   function paginatedFetch(
     characterURL = `https://rickandmortyapi.com/api/character/`,
@@ -20,7 +16,7 @@ function App() {
       return fetch(`${characterURL}?page=${page}`)
         .then(parse)
         .then(newResponse => {
-          console.log(newResponse.results)
+          // console.log(newResponse.results)
           const response = [...previousResponse, ...newResponse.results]
           
           if (newResponse.info.next !== null) {
@@ -33,6 +29,8 @@ function App() {
           
         })
     }
+
+
 
   //Use Effect to make the fetch
   useEffect(() => {
