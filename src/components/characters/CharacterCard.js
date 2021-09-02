1c4bf22907cbcd1794509c61af3419c8156579c5
eric <lee.eric.03@gmail.com>
2021-09-02 10:15:04 -0600

    refactors first and last page functions to work with pagination through array, instead of creating new component

--- src/components/characters/CharacterCard.js
+++ src/components/characters/CharacterCard.js
@@ -3,7 +3,7 @@ function CharacterCard({ character }) {
   return (
   <li className="flex flex-col items-center justify-center m-2">
     <h2 className="text-center">{ character.name }</h2>
-    <img src={ character.image } className="w-36 md:w-16 lg: "></img>
+    <img src={ character.image } className="w-36 md:w-16 rounded-lg"></img>
   </li>
   )}
 
--- src/components/characters/CharacterList.js
+++ src/components/characters/CharacterList.js
@@ -1,14 +1,9 @@
 import React, { useState } from 'react'
 import CharacterCard from './CharacterCard'
-import LastPage from './LastPage'
-import FirstPage from './FirstPage'
 
 function CharacterList({ characters }) {
   const [pageNumber, setPageNumber] = useState(0)
   const [pageSize, setPageSize] = useState(6)
-  const [showMainPage, setShowMainPage] = useState(true)
-  const [showLastPage, setShowLastPage] = useState(false)
-  const [showFirstPage, setShowFirstPage] = useState(false)
 
   let totalPages = Math.ceil(characters.length / pageSize)
 
@@ -19,57 +14,48 @@ function CharacterList({ characters }) {
     return characters.slice(arrayStart, arrayEnd)
   }
 
-  // renders first 6 characters as cards on page open 
+  // renders 6 characters as cards, at page open first 6 characters
   const createCard = () => paginate(characters, pageSize, pageNumber).map(character => { 
     return (
       <CharacterCard character={ character } key={ character.id } />
     )
   })
 
-  const createLastSet = () => paginate(characters, pageSize, totalPages).map(character => {
-      return (<CharacterCard character={ character } key={ character.id } />
-      )
-    })
+    const nextPage = () => {
+      // this ternary prevents the increment counter from going above 1 when paginating forward
+      return pageNumber === totalPages - 1 ?
+        pageNumber :
+        setPageNumber(pageNumber + 1)
+    }
 
-  const prevPage = () => {
-    // This ternary prevents the decrement counter from going below 0 when paginating backward
-    return pageNumber === 0 ?
+    const prevPage = () => {
+      // This ternary prevents the decrement counter from going below 0 when paginating backward
+      return pageNumber === 0 ?
+        pageNumber :
+        setPageNumber(pageNumber - 1)
+    }
+
+    const jumpToLast = () => {
+      return pageNumber === totalPages ?
       pageNumber :
-      setPageNumber(pageNumber - 1)
-  }
+      setPageNumber(totalPages - 1) 
+    }
 
-  const nextPage = () => {
-    // this ternary prevents the increment counter from going above 1 when paginating forward
-    return pageNumber === totalPages ?
+    const jumpToFirst = () => {
+      return pageNumber === 0 ?
       pageNumber :
-      setPageNumber(pageNumber + 1)
-  }
-
-  const onClickFirst = () => {
-    setShowMainPage(false)
-    setShowFirstPage(true)
-    setShowLastPage(false)
-    // setPageNumber(0)
-  }
-  const onClickLast = () => { 
-    setShowMainPage(false)
-    setShowFirstPage(false)
-    setShowLastPage(true)
-    setPageNumber(totalPages)
-  }
+      setPageNumber(0)
+    }
 
   return (
     <div className="w-full h-1/4 p-1.5 bg-gray-100">
-      {/* <LastPage lastPage={ characters.slice(-7,-1)}/> */}
       <ul className="grid grid-cols-2">      
-        { showMainPage ? createCard() : null}
-        {showLastPage ? createLastSet() : null }
-        {showFirstPage ? <FirstPage firstPage={ characters.slice(0,6)}/> : null}
+        {createCard()}
       </ul>
-      <div className="flex flex-row items-center justify-center">
+      <div className="flex flex-row items-center justify-center m-2">
         <button type="button" onClick={prevPage} className="border-2 border-black m-1">prev</button> 
-        <button type="button" onClick={onClickFirst}>First</button>
-        <button type="button" onClick={onClickLast}>Last</button>
+        <button type="button" onClick={jumpToFirst}>First</button>
+        <button type="button" onClick={jumpToLast}>Last</button>
         <button type="button" onClick={nextPage} className="border-2 border-black m-1">next</button> 
       </div>
     </div>
