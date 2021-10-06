1f91af8187542ea0b53f71c20ddcb335bb3b6ea2
eric <lee.eric.03@gmail.com>
2021-10-05 20:33:14 -0600

    filters locations based on characters pulled, removes duplicates

--- src/locations/LocationList.js
+++ src/locations/LocationList.js
@@ -1,16 +1,27 @@
 import react, { useState, useEffect } from 'react'
 
 function LocationList({ characters }) {
+  const [location, setLocation] =  useState([])
+
   const parse = response => response.json()
 
   function showLocations(characters) {
+    let locationArr = []
     for(let i = 0; i < characters.length; i++) {
-      console.log(characters[i].location)
-      }
+      locationArr.push(characters[i].location)
     }
 
-  showLocations(characters)
+    // function removeDuplicates(arr) {
+      let filteredArr = locationArr.filter((locationArr, index, self) => index === self.findIndex((t) => (t.name === locationArr.name)))
+    // }
+    console.log(filteredArr)
+    // console.log(filteredArr)
+    // return locationArr
+    }
 
+    showLocations(characters)
+    
+    
   return (
     <div className="flex-col min-w-screen min-h-full p-1.5 mt-10 bg-blue-100 shadow-lg overflow-hidden">
       <ul>
