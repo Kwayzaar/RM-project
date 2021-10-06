988d3a9156ed6b765951adc74863402f8e8b4ad5
eric <lee.eric.03@gmail.com>
2021-10-05 21:44:05 -0600

    fixes inifinte loop when invoking useEffect to filter out location duplicates. getLocations is fully functional and setting state correctly

--- src/locations/LocationList.js
+++ src/locations/LocationList.js
@@ -2,25 +2,28 @@ import React, { useState, useEffect } from 'react'
 
 function LocationList({ characters }) {
   const [locations, setLocations] = useState([])
+  const [locationId, setLocationId] = useState(0)
 
   function getLocations(characters) {
-    
+
     let locationArr = []
     for(let i = 0; i < characters.length; i++){
       locationArr.push(characters[i].location)
     }
 
     // filter the location array to remove duplicates 
-    let filteredArr = locationArr.filter((locationArr, index, self) => index === self.findIndex((t) => (t.name === locationArr.name)))
+    const filteredArr = locationArr.filter((locationArr, index, self) => index === self.findIndex((t) => (t.name === locationArr.name)))
 
-  // return setLocations(filteredArr)
+    return setLocations(filteredArr)
   }
+
+  console.log(locations)
 
   useEffect(() => {
     getLocations(characters)
-  })
+  }, [locationId])
 
-  console.log(locations)
+  // console.log(locations)
 
   return (
     <div className="flex-col min-w-screen min-h-full p-1.5 mt-10 bg-blue-100 shadow-lg overflow-hidden">
