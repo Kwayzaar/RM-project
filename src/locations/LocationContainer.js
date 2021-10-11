39a2512d8f3e0ad197f070231bca5f950d0c5661
eric <lee.eric.03@gmail.com>
2021-10-10 19:49:02 -0600

    cleans up filter function for location array

--- src/locations/LocationContainer.js
+++ src/locations/LocationContainer.js
@@ -3,31 +3,33 @@ import LocationButtons from './LocationButtons'
 
 function LocationContainer({ characters }) {
   const [locations, setLocations] = useState(characters)
-  const [locationId, setLocationId] = useState(0)
+  const [filteredArr, setFilteredArr] = useState([])
 
   useEffect(() => {
     setLocations(characters)
     // filterArr(locations)
   }, [characters])
-  
-  // filter the location array to remove duplicates 
-  function filterArr(arr) {
-    locationArr = arr.filter((arr, index, self) => index === self.findIndex((t) => (t.name === arr.name)))
-    return arr
-  }
 
-  // setLocations(characters)
 // This function gets locations based on the characters fetched and pushes into new array. Also removes location duplicates 
-  // function getLocations(arr) {
-  //     let locationArr = []
-  //     for(let i = 0; i < arr.length; i++){
-  //       locationArr.push(arr[i].location)
-  //     }
-  //     return setLocations(locationArr)
-  // }
+  function getLocations(arr) {
+      let locationArr = []
+      for(let i = 0; i < arr.length; i++){
+        locationArr.push(arr[i].location)
+      }
+      // console.log(locationArr)
+      // return setLocations(locationArr)
 
-
-
+      // filter the location array to remove duplicates 
+    const filterArr = (arr) => {
+      let localArr = arr
+      localArr = arr.filter((arr, index, self) => index === self.findIndex((t) => (t.name === arr.name)))
+      console.log(localArr)
+      // return setFilteredArr(arr)
+    }
+    filterArr(locationArr)
+  }
+  getLocations(locations)
+  
   const createLocationButtons = () => locations.map(location => {
     return <LocationButtons location={ location.name } key={ location.id }/>
   })
