b069d3b34fb941b8bc48832b49caeb616409c791
eric <lee.eric.03@gmail.com>
2021-10-27 11:55:46 -0600

    renames CharacterContainer to CharacterPopup

---
+++ src/components/characterProfile/CharacterPopup.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function CharacterPopup() {
+  reutrn (
+    <div className="flex flex-col">
+      <div>
+        <button>Back</button>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default CharacterPopup;
